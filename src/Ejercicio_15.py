790212085b5fffbec7da29f4ac7a2925b6b00639
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-19 20:14:46 +0200

    Cambios realizados Ejercicio_14 y test_ej14.py y Realización de Ejercicio_15.py y del test_ej15.py

--- src/Ejercicio_15.py
+++ src/Ejercicio_15.py
@@ -1,7 +1,7 @@
 # Leer números enteros de teclado, hasta que el usuario ingrese el 0. 
-# Finalmente, mostrar la sumatoria de todos los números ingresados.
+# Finalmente, mostrar la sumatoria de todos los números positivos ingresados.
 
-def sumatorioNum():
+def sumatorioNumPos():
     sumatoria = 0
     salir = False
     while not salir:
@@ -9,9 +9,14 @@ def sumatorioNum():
             numero_usuario = input("Escribe cualquier número entero (o 0 para salir): ")
             numero = int(numero_usuario)
         except ValueError:
-            print("Entrada inválida. Introduce un número entero.")
+            print("Entrada inválida. Introduce un número entero positivo.")
             continue
-        sumatoria += numero
+
+        if numero < 0:
+            print("No se pueden introducir números negativos. Introduce un número entero positivo.")
+        else:
+            sumatoria += numero
+
         if numero == 0:
             salir = True
     
@@ -19,6 +24,7 @@ def sumatorioNum():
     return mensaje
 
 if __name__ == "__main__":
-    # Procesamiento y salida
-    resultado = sumatorioNum()
+    # Procesamiento
+    resultado = sumatorioNumPos()
+    # Salida
     print(resultado)
--- test/test_ej14.py
+++ test/test_ej14.py
@@ -9,7 +9,7 @@ def test_sumatorio_numeros_positivos(mock_input):
 @patch("builtins.input", side_effect=["-5", "0"])
 def test_sumatorio_numero_negativo(mock_input):
     resultado = sumatorioNum()
-    assert resultado == "La sumatoria de los números ingresados es: 0"
+    assert resultado == "La sumatoria de los números ingresados es: -5"
 
 @patch("builtins.input", side_effect=["abc", "2", "0"])
 def test_sumatorio_entrada_invalida(mock_input):
