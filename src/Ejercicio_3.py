dcdac8ea12975c0385a4eb7200280998cca2bff7
rubencelislobato@gmail.com <rubencelislobato@gmail.com>
2023-10-19 11:44:53 +0200

    Cambios Realizados Ejercicio_3.py

--- src/Ejercicio_3.py
+++ src/Ejercicio_3.py
@@ -2,20 +2,27 @@
 # pantalla todos los números impares desde 1 hasta ese número separados por comas.
 
 def numImpares(numPos):
+    if not isinstance(numPos, int):
+        return "Por favor, ingresa un número entero positivo."
+    if numPos <= 0:
+        return []
     impares = []
     for numero in range(1, numPos + 1, 2):
         impares.append(numero)
     return impares
 
 if __name__ == "__main__":
-    while True:
+    numPos = 0
+    while numPos <= 0:
         try:
             numPos = int(input("Escribe un número entero positivo: "))
             if numPos <= 0:
                 print("Por favor, ingresa un número entero positivo.")
-            else:
-                impares = numImpares(numPos)
-                print(", ".join(map(str, impares)))
-                break
         except ValueError:
             print("Por favor, ingresa un número entero positivo válido.")
+    
+    impares = numImpares(numPos)
+    if isinstance(impares, list):
+        print(", ".join(map(str, impares)))
+    else:
+        print(impares)
