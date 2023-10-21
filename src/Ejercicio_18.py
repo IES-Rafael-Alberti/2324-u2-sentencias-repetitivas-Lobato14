3d084441b5858170f3fb89e695abac0bedbeed74
Rubén <rubencelislobato@gmauil.com>
2023-10-21 13:30:10 +0200

    Cambios realizados en Ejercicio_18.py y en test_ej18.py

--- src/Ejercicio_18.py
+++ src/Ejercicio_18.py
@@ -4,27 +4,39 @@
 # el usuario fueron números pares.
 
 def suma_digitos(numero):
-    return sum(int(digito) for digito in str(numero))
+    try:
+        numero = int(numero)
+        if numero < 0:
+            return "Error, el número debe ser un entero positivo."
+        return sum(int(digito) for digito in str(numero))
+    except ValueError:
+        return "Error, por favor ingrese un número entero válido."
+
+def contar_numeros_pares(numeros):
+    count = 0
+    for numero in numeros:
+        if numero < 0:
+            return "Error, el número debe ser un entero positivo."
+        if numero % 2 == 0:
+            count += 1
+    return count
 
 if __name__ == "__main__":
-    # Entrada 1
     numeros_pares = 0
-    numero = 0
-    # Proceso 1
-    while numero != -1:
-        # Entrada 2
-        numero = int(input("Ingrese un número entero positivo (-1 para salir): "))
-        # Proceso 2
-        if numero > 0:
-            # Proceso 2
-            suma = suma_digitos(numero)
-            # Salida 1
+    numeros_ingresados = []
+    numero_str = input("Ingrese un número entero positivo (-1 para salir): ")
+    while numero_str != "-1":
+        if numero_str.isdigit():
+            suma = suma_digitos(numero_str)
             print("La suma de los dígitos es:", suma)
-            # Proceso 3
-            if numero % 2 == 0:
-                numeros_pares += 1
-        elif numero != -1:
-            # Salida 2
-            print("Error, el número debe ser un entero positivo.")
-    # Salida 3
-    print("Cantidad de números pares ingresados:", numeros_pares)
+            if isinstance(suma, int):
+                numero = int(numero_str)
+                numeros_ingresados.append(numero)
+                if numero % 2 == 0:
+                    numeros_pares += 1
+            else:
+                print(suma)
+        else:
+            print("Error, por favor ingrese un número entero válido.")
+        numero_str = input("Ingrese un número entero positivo (-1 para salir): ")
+    print("Cantidad de números pares ingresados:", contar_numeros_pares(numeros_ingresados))
--- test/test_ej18.py
+++ test/test_ej18.py
@@ -1,11 +1,11 @@
-from src.Ejercicio_18 import suma_digitos
+from src.Ejercicio_18 import suma_digitos, contar_numeros_pares
 
 def test_suma_digitos():
     assert suma_digitos(123) == 6
-    assert suma_digitos(456) == 15
-    assert suma_digitos(789) == 24
-    assert suma_digitos(0) == 0
-    assert suma_digitos(1) == 1
-    assert suma_digitos(11) == 2
-    assert suma_digitos(111) == 3
-    assert suma_digitos(123456789) == 45
+    assert suma_digitos(-456) == "Error, el número debe ser un entero positivo."
+    assert suma_digitos("4-56") == "Error, por favor ingrese un número entero válido."
+    assert suma_digitos("48-6") == "Error, por favor ingrese un número entero válido."
+
+def test_contar_numeros_pares():
+    assert contar_numeros_pares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 5
+    assert contar_numeros_pares([1, -3, 5, 7, 9]) == "Error, el número debe ser un entero positivo."
