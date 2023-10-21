2ec877f952c183680f08d00a1dc7bd275bc913f6
Rubén <rubencelislobato@gmauil.com>
2023-10-21 23:16:50 +0200

    Cambios realizados Ejercicio_4.py y test_ej4.py

--- src/Ejercicio_4.py
+++ src/Ejercicio_4.py
@@ -3,12 +3,8 @@
 # separados por comas.
 
 def contar_atras(numPos):
-    numPos = str(numPos)
-    while not numPos.isdigit() or int(numPos) <= 0:
-        numPos = input("Escribe un número entero positivo: ")
-    numPos = int(numPos)
     resultado = ""
-    for numero in range(numPos, -1, -1):
+    for numero in range(int(numPos), -1, -1):
         resultado += str(numero)
         if numero != 0:
             resultado += ", "
@@ -17,5 +13,7 @@ def contar_atras(numPos):
 if __name__ == "__main__":
     # Entrada
     numPos = input("Escribe un número entero positivo: ")
+    while not numPos.isdigit() or int(numPos) <= 0:
+        numPos = input("Escribe un número entero positivo: ")
     # Salida
     print(contar_atras(numPos))
--- test/test_ej4.py
+++ test/test_ej4.py
@@ -2,6 +2,5 @@ from src.Ejercicio_4 import contar_atras
 
 def test_contar_atras_numero_positivo():
     assert contar_atras(5) == "5, 4, 3, 2, 1, 0"
-    assert contar_atras("Hola") == "Escribe un número entero positivo: "
-    assert contar_atras(0) == "Escribe un número entero positivo: "
-    assert contar_atras(-5) == "Escribe un número entero positivo: "
+    # assert contar_atras("Hola") == "Escribe un número entero positivo: "
+    assert contar_atras(0) == "0"
